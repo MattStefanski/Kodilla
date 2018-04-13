d1a77ab6703602f014decac28e0087e3b17c91a2
MattStefanski <mateusz.stefanski.eu@gmail.com>
2018-04-14 01:09:35 +0200

    calc adding

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
-
+import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calc=new Calculator();
+
+
+        if (calc.add(1,2.21)==3.21){
+            System.out.println("Adding ok");
+        } else {
+            System.out.println("Adding Error!");
+        }
+
+
+
+
+        if (calc.substract(2.21,1.00)==1.21){
+            System.out.println("Substracting ok");
+        } else {
+            System.out.println("Substracting Error!");
+        }
     }
 }
