2b438a9515baf5a3f2fb729022453ca658590e94
MattStefanski <mateusz.stefanski.eu@gmail.com>
2018-04-19 20:51:42 +0200

    PoemBeautifier

---
+++ kodilla-testing/src/main/java/com/kodilla/stream/PoemDecorator.java
@@ -0,0 +1,14 @@
+/*
+ * Created by Matt Stefanski on 4/18/18 11:37 PM
+ *
+ * Copyright (c) 2018. All rights reserved
+ *
+ * Last modified 4/18/18 11:37 PM
+ */
+
+package com.kodilla.stream;
+
+public interface PoemDecorator {
+    void decorate(String text);
+
+}
