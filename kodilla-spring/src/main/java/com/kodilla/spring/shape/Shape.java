eaf07b5f1f45523eb7bbc384738d63ad53e800d9
MattStefanski <mateusz.stefanski.eu@gmail.com>
2018-05-09 14:24:12 +0200

    Adding first spring app

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/shape/Shape.java
@@ -0,0 +1,14 @@
+/*
+ * Created by Matt Stefanski on 5/9/18 2:15 PM
+ *
+ * Copyright (c) 2018. All rights reserved
+ *
+ * Last modified 5/9/18 2:15 PM
+ */
+
+package com.kodilla.spring.shape;
+
+
+public interface Shape {
+    String getShapeName();
+}
