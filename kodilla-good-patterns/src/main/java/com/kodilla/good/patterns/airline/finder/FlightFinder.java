17ccd0e91dd930e3787a1b3209557c140f32a393
MattStefanski <mateusz.stefanski.eu@gmail.com>
2018-05-09 15:49:58 +0200

    reventing

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/airline/finder/FlightFinder.java
@@ -0,0 +1,51 @@
+/*
+ * Created by Matt Stefanski on 5/8/18 8:14 PM
+ *
+ * Copyright (c) 2018. All rights reserved
+ *
+ * Last modified 5/8/18 8:05 PM
+ */
+
+package com.kodilla.good.patterns.airline.finder;
+
+
+import com.kodilla.good.patterns.airline.schedule.FlightSchedule;
+
+public class FlightFinder {
+
+
+    public void listByDepartureAirport(FlightSchedule schedule, String departureAirport) {
+
+        schedule.getSchedule()
+                .entrySet()
+                .stream()
+                .filter(m -> m.getValue().getConnection().getDepartureAirport().equals(departureAirport))
+                .forEach(m -> System.out.println(m.getValue().toString()));
+
+    }
+
+
+    public void listByArrivalAirport(FlightSchedule schedule, String arrivalAirport) {
+
+        schedule.getSchedule()
+                .entrySet()
+                .stream()
+                .filter(m -> m.getValue().getConnection().getArrivalAirport().equals(arrivalAirport))
+                .forEach(m -> System.out.println(m.getValue().toString()));
+
+    }
+
+    public void listByStopAirport(FlightSchedule schedule,String departureAirport, String arrivalAirport, String stopAirport) {
+
+        schedule.getSchedule()
+                .entrySet()
+                .stream()
+                .filter(m -> m.getValue().getConnection().getDepartureAirport().equals(departureAirport))
+                .filter(m -> m.getValue().getConnection().getArrivalAirport().equals(arrivalAirport))
+                .filter(m -> m.getValue().getConnection().getStops().contains(stopAirport))
+                .forEach(m -> System.out.println(m.getValue().toString()));
+
+    }
+
+
+}
