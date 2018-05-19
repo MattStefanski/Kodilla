043678a24962b33a30bd76092412e559058f5dc6
MattStefanski <mateusz.stefanski.eu@gmail.com>
2018-05-19 13:13:32 +0200

    Adding task3 Module11

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/tools/FacebookPublisher.java
@@ -0,0 +1,17 @@
+/*
+ * Created by Matt Stefanski on 5/19/18 1:01 PM
+ *
+ * Copyright (c) 2018. All rights reserved
+ *
+ * Last modified 5/19/18 12:45 PM
+ */
+
+package com.kodilla.patterns.strategy.social.tools;
+
+public class FacebookPublisher implements  SocialPublisher {
+
+    @Override
+    public String share() {
+        return  "Sharing by Facebook";
+    }
+}
---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/user/Millenials.java
@@ -0,0 +1,19 @@
+/*
+ * Created by Matt Stefanski on 5/19/18 1:00 PM
+ *
+ * Copyright (c) 2018. All rights reserved
+ *
+ * Last modified 5/19/18 12:43 PM
+ */
+
+package com.kodilla.patterns.strategy.social.user;
+
+import com.kodilla.patterns.strategy.social.tools.TwitterPublisher;
+
+public class Millenials extends User {
+
+    public Millenials(String username) {
+        super(username);
+        this.socialPublisher = new TwitterPublisher();
+    }
+}
