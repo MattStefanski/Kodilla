f9edd5c8bccc04f4bb5813a20767858c48ec0376
Mateusz Stefanski <mateusz.stefanski.eu@gmail.com>
2018-08-18 12:03:58 +0200

    Adding adapter

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/libraryb/BookSignature.java
@@ -0,0 +1,22 @@
+/*
+ * Created by Matt Stefanski on 8/18/18 11:14 AM
+ *
+ * Copyright (c) 2018. All rights reserved
+ *
+ * Last modified 8/18/18 11:14 AM
+ */
+
+package com.kodilla.patterns2.adapter.bookclassifier.libraryb;
+
+public class BookSignature {
+    private final String signature;
+
+    public BookSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+}
